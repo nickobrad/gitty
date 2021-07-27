5beded761d2ecc447455b0bc17d9fc4537c27cff
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-27 16:48:48 +0300

    Commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RepoSearchComponent } from './repo-search/repo-search.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'user-search', component: UserSearchComponent },
+  { path: 'repo-search', component: RepoSearchComponent},
+  // { path: '', redirectTo:"homepage", pathMatch:"full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
