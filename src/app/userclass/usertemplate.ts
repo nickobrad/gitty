37ef9bbcf83ace5a25559a2a1d996cb69b4a2f24
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-27 16:53:27 +0300

    Commit 23

--- src/app/userclass/usertemplate.ts
+++ src/app/userclass/usertemplate.ts
@@ -1,2 +1,16 @@
 export class Usertemplate {
+    constructor(
+        public avatar_url: any,
+        public id: any,
+        public login: string,
+        public name: string,
+        public location: string,
+        public bio: string,
+        public followers: number,
+        public html_url: string,
+        public created_at: Date,
+        public public_repos: any
+    ){
+
+    }
 }
