155d000881369f932fa5b06f7d24574f02c3a7b1
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-27 16:52:00 +0300

    Commit 14

--- src/app/services/allservices.service.ts
+++ src/app/services/allservices.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AllservicesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+    
+  }
+
+  getTheUser(inputValue: string){
+    return this.http.get(`https://api.github.com/users/${inputValue}`);
+  }
+
+  getTheUserRepo(inputValue: string){
+    return this.http.get(`https://api.github.com/users/${inputValue}/repos`);
+  }
+
+  searchForRepository(inputValue: string){
+    return this.http.get(` https://api.github.com/search/repositories?q=${inputValue}`);
+  }
 }
