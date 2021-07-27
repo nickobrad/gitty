82e40cf6e265195f4e05ef4dadca82123905deae
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-27 16:53:13 +0300

    Commit 21

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AllservicesService } from '../services/allservices.service';
+import { Usertemplate } from '../userclass/usertemplate';
+import { Repotemplate } from '../repoclass/repotemplate';
 
 @Component({
   selector: 'app-user-search',
@@ -7,7 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSearchComponent implements OnInit {
 
-  constructor() { }
+  showResults = true;
+
+  user!: Usertemplate;
+  repos!: Repotemplate[]; 
+ 
+  constructor(
+    private http: HttpClient,
+    private allServices: AllservicesService 
+  ) 
+  { }
+
+    lookForUser(input:string){
+      this.allServices.getTheUser(input).toPromise().then(
+        (response:any) => {
+          this.user = response;
+          this.allServices.getTheUserRepo(input).subscribe(
+            (feedback:any) => {
+              this.repos = feedback;
+              console.log(this.repos)
+            }
+          )
+        }, (error: any) =>{
+          this.allServices.getTheUser('nickobrad');
+        }
+      )
+    }
+
+    // repoGet(){
+    //   this.allServices.getTheUserRepo('nickobrad').subscribe(
+    //     (feedback:any) => {
+    //       this.repos = feedback;
+    //       console.log(this.repos)
+    //     }
+    //   )
+    // }
+
+    toggleDisplay(){
+      this.showResults = !this.showResults;
+    }
 
   ngOnInit(): void {
   }
