2e37a419c5ffa29db3542d4b0db0d41602d27240
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-27 16:51:38 +0300

    Commit 12

--- src/app/repo-search/repo-search.component.ts
+++ src/app/repo-search/repo-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AllservicesService } from '../services/allservices.service';
+import { Usertemplate } from '../userclass/usertemplate';
+import { Repotemplate } from '../repoclass/repotemplate';
 
 @Component({
   selector: 'app-repo-search',
@@ -6,8 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-search.component.css']
 })
 export class RepoSearchComponent implements OnInit {
+  showResults = true;
 
-  constructor() { }
+  user!: Usertemplate;
+  repos: any [] = [];
+ 
+  constructor(
+    private http: HttpClient,
+    private allServices: AllservicesService 
+  ) 
+  { }
+
+    lookForRepo(input:string){
+      this.allServices.searchForRepository(input).toPromise().then(
+        (response:any) => {
+          this.repos = response.items;
+          console.log(response)
+          
+        }, (error: any) =>{
+          this.allServices.getTheUser('nickobrad');
+        }
+      )
+    }
+
+    toggleDisplay(){
+      this.showResults = !this.showResults;
+    }
 
   ngOnInit(): void {
   }
