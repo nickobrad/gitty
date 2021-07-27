7282bb75b70c6d3ef7658bfe05bf07595ad39245
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-27 16:51:50 +0300

    Commit 13

--- src/app/repoclass/repotemplate.ts
+++ src/app/repoclass/repotemplate.ts
@@ -1,2 +1,14 @@
 export class Repotemplate {
+    constructor(
+        public id: any,
+        public name: string,
+        public description: string,
+        public html_url: string,
+        public forks: number,
+        public language: [],
+        public url: any,
+        public created_at: Date
+    ){
+
+    }
 }
