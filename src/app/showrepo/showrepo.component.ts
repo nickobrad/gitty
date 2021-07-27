b6a4f52676aaa972449474276d6d874def1f05ad
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-27 16:52:42 +0300

    Commit 18

---
+++ src/app/showrepo/showrepo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showrepo',
+  templateUrl: './showrepo.component.html',
+  styleUrls: ['./showrepo.component.css']
+})
+export class ShowrepoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
